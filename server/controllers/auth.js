717b025237f14a8435690b4bc4624080d2a564f9
Tim-KS <tim.kandan-smith@outlook.com>
2023-01-09 21:54:36 +1100

    Added JWT auth middleware

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -40,10 +40,10 @@ export const login = async (req, res) => {
     try {
         const { email, password } = req.body;
         const user = await User.findOne({ email: email });
-        if (!user) return res.status(400).json({ msg: "Invalid Login! " });
+        if (!user) return res.status(400).json({ msg: "Invalid Login." });
 
         const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) return res.status(400).jso({ msg: "Invalid Login! " });
+        if (!isMatch) return res.status(400).jso({ msg: "Invalid Login." });
 
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
 
