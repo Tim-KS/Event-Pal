f7ce0cfe562e8dd232eef12f73041aa1e7066e62
Jonathan <jonathanwijayan@gmail.com>
2023-01-09 21:35:56 +1100

    Added user schema and added authentication

---
+++ server/models/User.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            require: true,
+            min: 2,
+            max: 50,
+        },
+        lastName: {
+            type: String,
+            require: true,
+            min: 2,
+            max: 50,
+        },
+        email: {
+            type: String,
+            require: true,
+            min: 2,
+            max: 50,
+            unique: true,
+        },
+        friends: {
+            type: Array,
+            default: [],
+        },
+        phoneNumber: {
+            type: String,
+            require: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            require: true,
+            min: 8,
+        },
+        interest: {
+            type: String,
+            require: true,
+        },
+        picturePath: {
+            type: String,
+            default: " ",
+        },
+    },
+    { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+export default User;
