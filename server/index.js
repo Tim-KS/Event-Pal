6a5b63718ce22a47910ecced02452ba5530cc7ae
Tim-KS <tim.kandan-smith@outlook.com>
2023-01-09 20:30:48 +1100

    MongoDB server and connection succesful

--- server/index.js
+++ server/index.js
@@ -33,3 +33,15 @@ const storage = multer.diskStorage({
     },
 });
 const upload = multer({ storage });
+
+//Mongoose Setup
+const PORT = process.env.PORT || 6001;
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        app.listen(PORT, () => console.log(`Listening on server port: ${PORT}`));
+    })
+    .catch((error) => console.log(`${error} did not connect`));
