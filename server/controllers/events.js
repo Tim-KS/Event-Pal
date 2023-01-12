00779d9167d9f9be35f21af0190aa83c6c29c269
Tim-KS <tim.kandan-smith@outlook.com>
2023-01-12 17:07:16 +1100

    Added register and login forms and debugged server

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -32,4 +32,41 @@ export const getFeedEvents = async (req, res) => {
     } catch (err) {
         res.status(404).json({ message: err.message })
     }
-}
+};
+
+export const getUserEvents = async (req, res) => {
+    try {
+      const { userId } = req.params;
+      const event = await Event.find({ userId });
+      res.status(200).json(event);
+    } catch (err) {
+      res.status(404).json({ message: err.message });
+    }
+  };
+
+  /* UPDATE */
+export const likeEvents = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { userId } = req.body;
+      const event = await Event.findById(id);
+      const isLiked = event.likes.get(userId);
+  
+      if (isLiked) {
+        event.likes.delete(userId);
+      } else {
+        event.likes.set(userId, true);
+      }
+  
+      const updatedEvent = await Event.findByIdAndUpdate(
+        id,
+        { likes: event.likes },
+        { new: true }
+      );
+  
+      res.status(200).json(updatedEvent);
+    } catch (err) {
+      res.status(404).json({ message: err.message });
+    }
+  };
+  
